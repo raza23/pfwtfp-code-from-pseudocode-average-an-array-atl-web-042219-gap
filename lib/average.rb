f9ceb045cf128af14c731f517acb801899a5b61f
Steven G. Harms <steven.harms@flatironschool.com>
2018-10-26 10:39:07 -0400

    solution

--- lib/average.rb
+++ lib/average.rb
@@ -1,1 +1,9 @@
-
+def average_numbers(collection)
+  accumulator = 0.0
+  if !collection.empty?
+    collection.each do |n|
+      accumulator += n
+    end
+    accumulator / collection.count
+  end
+end
