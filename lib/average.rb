c5b5a2f14f8be2bc09dbb974a81ca0d800fcea02
Raza <rtshareef@gmail.com>
2019-08-12 18:06:06 -0400

    Done.

--- lib/average.rb
+++ lib/average.rb
@@ -1,1 +1,16 @@
 # Add your code, translated from pseudocode, below!
+require 'pry'
+
+def average_numbers(collection)
+    collection_count = collection.count
+    if collection_count == 0
+         0
+    else running_total = 0
+        0.upto(collection.count - 1) do |i|
+            running_total += collection[i]
+        end
+         running_total / collection_count.to_f
+          
+    end
+
+  end
